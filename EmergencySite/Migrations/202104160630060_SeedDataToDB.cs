95fbc8b83be9ba05c4c6097e0759315662d8609f
Your Name <you@example.com>
2021-04-28 16:21:55 +0530

    seed vineet login details into logins

--- EmergencySite/Migrations/202104160630060_SeedDataToDB.cs
+++ EmergencySite/Migrations/202104160630060_SeedDataToDB.cs
@@ -8,8 +8,9 @@
         public override void Up()
         {
             //Sql("INSERT INTO [dbo].[Logins] ([CreatedAt],[Username],[Password],[EncryptedPassword])VALUES('2021-04-16 10:00:45.667', 'abhay.gupta@sisrtd.com', '12345', 'LKAekHU9EtweB49HAaTRfg==')");
+            //Sql("INSERT INTO [dbo].[Logins] ([CreatedAt],[Username],[Password],[EncryptedPassword])VALUES('2021-04-28 16:02:45.667', 'vineetshukla14u@gmail.com', 'vineet14u', '0qYedLcUB9O6lAXr9Z6egg====')");
         }
-        
+
         public override void Down()
         {
         }
